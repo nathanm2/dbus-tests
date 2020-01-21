68fc2b27b0dc02070b9ff719beaa2b2e1a35ab4e
Nathan Miller <Nathan.Miller@wdc.com>
2020-01-21 17:15:14 -0600

    Signal experiments

--- pydbus/test-client.py
+++ pydbus/test-client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from gi.repository import GLib
 from pydbus import SessionBus
 import argparse
 
@@ -12,6 +13,21 @@ def echo(service, args):
 def wait(service, args):
     service.Wait(args.timeout)
 
+def variant(service, args):
+    reply = service.VariantResponse(args.double)
+    print(reply)
+
+def send_signal(service, args):
+    service.SendSignal(args.name, args.value)
+
+def listen_signal(service, args):
+    bus = service._bus
+    callback = lambda sender, object, iface, signal, params: print("{} {}".format(signal, params))
+    bus.subscribe(sender=ENDPOINT, iface=ENDPOINT, signal=args.name, object=SERVICE_PATH,
+            arg0=args.value, signal_fired=callback)
+    loop = GLib.MainLoop()
+    loop.run()
+
 def main():
     parser = argparse.ArgumentParser(description="test service client")
     sub = parser.add_subparsers(title='subcommands', dest='subcommand')
@@ -23,6 +39,20 @@ def main():
     wait_parser = sub.add_parser("wait", help="simulate a long running command")
     wait_parser.add_argument("timeout", type=int, help="Amount of time to wait.")
     wait_parser.set_defaults(func=wait)
+
+    variant_parser = sub.add_parser("variant", help="returns a variant response")
+    variant_parser.add_argument("--double", "-d", action='store_true')
+    variant_parser.set_defaults(func=variant)
+
+    send_signal_parser = sub.add_parser("signal", help="send a signal")
+    send_signal_parser.add_argument("name", help="Signal name", choices=["S1", "S2"])
+    send_signal_parser.add_argument("value", help="Signal value")
+    send_signal_parser.set_defaults(func=send_signal)
+
+    listen_parser = sub.add_parser("listen", help="wait for a signal")
+    listen_parser.add_argument("--name", "-n", help="Signal name")
+    listen_parser.add_argument("--value", "-v", help="Signal value")
+    listen_parser.set_defaults(func=listen_signal)
 
     args = parser.parse_args()
 
--- pydbus/test-service.py
+++ pydbus/test-service.py
@@ -4,6 +4,7 @@ import time
 
 from gi.repository import GLib
 from pydbus import SessionBus
+from pydbus.generic import signal
 
 class TestService():
     """
@@ -20,6 +21,29 @@ class TestService():
           <!-- Appears to hang for the specified number of seconds. -->
           <arg type='t' name='timeout' direction='in'/>
         </method>
+
+        <method name='VariantResponse'>
+          <!-- Sends back a variant response. -->
+          <arg type='b' name='double' direction='in'/>
+          <arg type='v' name='result' direction='out'/>
+        </method>
+
+        <method name='SendSignal'>
+          <!-- Sends back a variant response. -->
+          <arg type='s' name='name' direction='in'/>
+          <arg type='s' name='value' direction='in'/>
+        </method>
+
+        <signal name="S1">
+          <!-- Signal 1 -->
+          <arg name="value1" type="s"/>
+        </signal>
+
+        <signal name="S2">
+          <!-- Signal 2 -->
+          <arg name="value1" type="s"/>
+        </signal>
+
       </interface>
     </node>
    """
@@ -31,6 +55,23 @@ class TestService():
         print("Wait: {}".format(timeout))
         time.sleep(timeout)
         print("Wait done.")
+
+    def VariantResponse(self, double):
+        print("VariantResponse: {}".format(double))
+        if double:
+            return GLib.Variant("(bs)", (double, "some string"))
+        else:
+            return GLib.Variant("(b)", (double,))
+
+    IfaceName = "com.wdc.TestService1"
+    S1 = signal()
+    S2 = signal()
+
+    def SendSignal(self, name, value):
+        if name == "S1":
+            self.S1(value)
+        else:
+            self.S2(value)
 
 def main():
     loop = GLib.MainLoop()
